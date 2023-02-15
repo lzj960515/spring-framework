596d3699a39848e32998fb82982852ab962d275b
liaozijian <lzj960515@163.com>
2023-02-15 17:20:49 +0800

    feat:测试

--- my-test/src/main/java/com/my/test/generic/GenericTest.java
+++ my-test/src/main/java/com/my/test/generic/GenericTest.java
@@ -2,9 +2,21 @@ package com.my.test.generic;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.annotation.AnnotatedBeanDefinitionReader;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.core.type.ClassMetadata;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.core.type.classreading.SimpleMetadataReaderFactory;
+
+import java.io.IOException;
+import java.util.Locale;
 
 /**
  * 测试Spring中的一些核心类
@@ -40,5 +52,52 @@ public class GenericTest {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
 		AnnotatedBeanDefinitionReader annotatedBeanDefinitionReader = new AnnotatedBeanDefinitionReader(context);
 		annotatedBeanDefinitionReader.register(UserService.class);
+		context.refresh();
+		UserService userService = context.getBean("userService", UserService.class);
+		Assertions.assertNotNull(userService);
+	}
+
+	@Test
+	public void testBeanFactory(){
+		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+		RootBeanDefinition rbd = new RootBeanDefinition(UserService.class);
+		beanFactory.registerBeanDefinition("userService", rbd);
+		UserService userService = beanFactory.getBean("userService", UserService.class);
+		Assertions.assertNotNull(userService);
+	}
+
+	@Test
+	public void testMessageSource(){
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("message");
+		Assertions.assertEquals("阿紫", messageSource.getMessage("name", null, Locale.getDefault()));
+		Assertions.assertEquals("azi", messageSource.getMessage("name", null, Locale.US));
+	}
+
+	@Test
+	public void testGetResource() throws IOException {
+		DefaultResourceLoader resourceLoader = new DefaultResourceLoader();
+		Resource resource = resourceLoader.getResource("https://zijiancode.cn");
+		resource = resourceLoader.getResource("classpath:com/my/test/generic/UserService.class");
+		Assertions.assertEquals("UserService.class", resource.getFilename());
+	}
+
+	@Test
+	public void testMetaReader() throws IOException {
+		SimpleMetadataReaderFactory simpleMetadataReaderFactory = new SimpleMetadataReaderFactory();
+
+		// 构造一个MetadataReader
+		MetadataReader metadataReader = simpleMetadataReaderFactory.getMetadataReader("com.my.test.generic.UserService");
+
+		// 得到一个ClassMetadata，并获取了类名
+		ClassMetadata classMetadata = metadataReader.getClassMetadata();
+
+		System.out.println(classMetadata.getClassName());
+
+		// 获取一个AnnotationMetadata，并获取类上的注解信息
+		AnnotationMetadata annotationMetadata = metadataReader.getAnnotationMetadata();
+		for (String annotationType : annotationMetadata.getAnnotationTypes()) {
+			System.out.println(annotationType);
+		}
 	}
 }
